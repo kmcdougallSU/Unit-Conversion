deadd9fa92605a43ae6883a35d1863f021776bf7
kmcdougallSU <kmcdougall@seattleu.edu>
2023-07-12 12:51:12 -1000

    convert from terminal output to swing

--- src/main/Conversions.java
+++ src/main/Conversions.java
@@ -1,20 +1,9 @@
-public class Conversions {
-    private static final double FEET_TO_METERS = 3.28,
-            FEET_TO_KILOMETER = 3281,
-            FEET_TO_CENTIMETER = 30.48,
-            FEET_TO_MILLIMETER = 304.8;
-
-    public double getFeetToMeters(int feet) {
-        return feet / FEET_TO_METERS;
+public final class Conversions {
+    public double convertFeetToMeters(double feet) {
+        return feet / 3.281;
     }
 
-    public double metersToFeet(int meter) {
-        return meter * FEET_TO_METERS;
+    public double convertMetersToFeet(double meters) {
+        return meters * 3.281;
     }
-
-    public double getFeetToCentimeter(int cm) {
-        return cm / FEET_TO_CENTIMETER;
-    }
-
-
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,36 +1,11 @@
+import javax.swing.*;
 import java.util.*;
 import java.text.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("#.##");
-        Conversions conversions = new Conversions();
-
-        System.out.print("""
-                Welcome to the unit converter program.
-                                
-                ************
-                1. feet
-                2. inches
-                ************
-                                
-                Enter corresponding unit menu number here:                
-                """);
-        int userSelection = keyboard.nextInt();
-        keyboard.nextLine();
-
-        switch (userSelection) {
-            case 1 -> feetConversions(df, keyboard, conversions);
-            case 2 -> inchConversions(df, keyboard, conversions);
-        }
-    }
-
-
-    public static void feetConversions(DecimalFormat df, Scanner keyboard, Conversions conversions) {
-        System.out.println("test");
-    }
-
-    private static void inchConversions(DecimalFormat df, Scanner keyboard, Conversions conversions) {
+    public static void main(String[] args) throws InterruptedException {
+        MainWindow test = new MainWindow();
+        test.getContentPane();
     }
 }
